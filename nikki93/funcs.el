97242c13f60b7e4717952c1fc75f7c59d65972ec
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-01-28 23:48:34 -0500

    add better eshell ag command

--- nikki93/funcs.el
+++ nikki93/funcs.el
@@ -14,9 +14,14 @@
 (defun nikki93/live-coding-window-setup ()
   (interactive)
   (nikki93/toggle-window-fixed
-    (split-window
-      (split-window (get-buffer-window (current-buffer)) -117 'right)
-      -47 'above)))
+   (split-window
+    (split-window (get-buffer-window (current-buffer)) -117 'right)
+    -47 'above)))
+
+(defun nikki93/flatten (x)
+  (cond ((null x) nil)
+        ((listp x) (append (nikki93/flatten (car x)) (nikki93/flatten (cdr x))))
+        (t (list x))))
 
 (defun nikki93/switch-fullscreen ()
   (interactive)
@@ -24,6 +29,17 @@
          (cm (cdr (assoc 'fullscreen (frame-parameters))))
          (next (cadr (member cm modes))))
     (modify-frame-parameters
-      (selected-frame)
-      (list (cons 'fullscreen next)))))
+     (selected-frame)
+     (list (cons 'fullscreen next)))))
+
+(defun eshell/ag (&rest args)
+  (let ((ag-highlight-search t))
+    (compilation-start
+     (mapconcat 'shell-quote-argument
+                (append (list ag-executable
+                              "--color" "--color-match" "30;43"
+                              "--smart-case" "--nogroup" "--column" "--")
+                        (nikki93/flatten args)) " ")
+     'ag-mode)))
+
 
