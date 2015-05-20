1d5519c250b31dee08a28e42abf8d4306c486b44
nikki93 <s.nikhilesh@gmail.com>
2015-05-20 00:15:41 -0400

    tex master

--- nikki93/packages.el
+++ nikki93/packages.el
@@ -89,6 +89,10 @@ which require an initialization must be listed explicitly in the list.")
     :defer t
     :config
     (progn
+      (setq TeX-auto-save t)
+      (setq TeX-parse-self t)
+      (setq-default TeX-master t)
+
       (defun auctex/build ()
         (interactive)
         (if (buffer-modified-p)
