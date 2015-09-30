cfca5b585b0d4f3e8f2b3687da741b4c9e56a4a0
Nikhilesh Sigatapu <s.nikhilesh@gmail.com>
2015-09-29 22:37:43 -0700

    buncha stuff

--- nikki93/keybindings.el
+++ nikki93/keybindings.el
@@ -1,6 +1,8 @@
 (define-key global-map [f11] 'nikki93/switch-fullscreen)
 (define-key global-map (kbd "C-s") 'save-buffer)
 (define-key global-map (kbd "M-s") 'save-buffer)
+(define-key global-map (kbd "M-`") 'delete-window)
+
 (define-key evil-normal-state-map ",r" 'nikki93/cgame-scratch)
 (define-key evil-visual-state-map ",r" 'nikki93/cgame-scratch)
 (define-key evil-normal-state-map ";r" 'nikki93/cgame-scratch)
